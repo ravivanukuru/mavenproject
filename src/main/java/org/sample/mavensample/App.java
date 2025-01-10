cc0761c8b00b7f19c3f7b3c721c0eef12b450b76
vanukuru.ravikiran <vanukuru.ravikiran@NA-JM67CY3-L.cnf.prod.cnf.com>
2025-01-09 18:28:37 -0800

    commit 2

--- src/main/java/org/sample/mavensample/App.java
+++ src/main/java/org/sample/mavensample/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "MAVEN PROJECT" );
+        System.out.println( "MAVEN PROJECT TEST" );
+        System.out.println( "MAVEN PROJECT TEST2" );
     }
 }
