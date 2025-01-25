986e159025105dbbc066404722bc9ea1dcf9a335
vanukuru.ravikiran <ravikiran.vanukuru@xpo.com>
2025-01-25 15:41:28 -0800

    pom updares

--- src/main/java/org/sample/mavensample/App.java
+++ src/main/java/org/sample/mavensample/App.java
@@ -1,11 +1,14 @@
 package org.sample.mavensample;
 
+import org.junit.Test;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
